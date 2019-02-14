3dbcc9a07770352f8320ec108fc5151c5517b131
Karan krishna <mail2karankrishnan@gmail.com>
2019-02-14 11:22:31 +0530

    Implementation of flask api

--- Neena/program.py
+++ Neena/program.py
@@ -11,6 +11,14 @@ from selenium import webdriver
 import re
 import urllib.request
 import dns.resolver
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+CORS(app)
+
+
 ## Var declares
 
 #COMMON
@@ -45,48 +53,76 @@ cors_cmd_str2=' ./corstest.py -q outfile.cors'
 
 
 def xmas(hostname):
-    d=os.popen(xmas_cmd_str1+ hostname+xmas_cmd_str2).read()
-    d= d[:-1].split(newLine)
+    try:
+        d=os.popen(xmas_cmd_str1+ hostname+xmas_cmd_str2).read()
+        d= d[:-1].split(newLine)
+    except Exception as e:
+        print(str(e)+"xmas")
+        d=""
     return d
 
 def XSSProtection(hostname):
-    r = requests.head(http_var+hostname, allow_redirects=True)
-    d=os.popen(XSSProtection_cmd_str1+ r.url+XSSProtection_cmd_str2).read()
-    XSSProtection = (XSSProtection_indicator_str1 in d)
+    try:
+        r = requests.head(http_var+hostname, allow_redirects=True)
+        d=os.popen(XSSProtection_cmd_str1+ r.url+XSSProtection_cmd_str2).read()
+        XSSProtection = (XSSProtection_indicator_str1 in d)
+    except Exception as e:
+         print(str(e)+"XSSProtection")
+         XSSProtection=False
     return XSSProtection
 
 def XFrameOptions(hostname):
-    r = requests.head(http_var+hostname, allow_redirects=True)
-    d=os.popen(curl_header+ r.url+XFrameOptions_cmd_str1).read()
-    XFrameOptions = (XFrameOptions_indicator_str1 in d or XFrameOptions_indicator_str2 in d)
+    try:
+        r = requests.head(http_var+hostname, allow_redirects=True)
+        d=os.popen(curl_header+ r.url+XFrameOptions_cmd_str1).read()
+        XFrameOptions = (XFrameOptions_indicator_str1 in d or XFrameOptions_indicator_str2 in d)
+    except Exception as e:
+         print(str(e)+"XFrameOptions")
+         XFrameOptions=False
     return XFrameOptions
     
 def XContentTypeOptions(hostname):
-    r = requests.head(http_var+hostname, allow_redirects=True)
-    d=os.popen(curl_header+ r.url+XContentTypeOptions_cmd_str1).read()
-    XContentTypeOptions = (XContentTypeOptions_indicator_str1 in d )
+    try:
+        r = requests.head(http_var+hostname, allow_redirects=True)
+        d=os.popen(curl_header+ r.url+XContentTypeOptions_cmd_str1).read()
+        XContentTypeOptions = (XContentTypeOptions_indicator_str1 in d )
+    except Exception as e:
+        print(str(e)+"XContentTypeOptions")
+        XContentTypeOptions=False
     return XContentTypeOptions
 
 def cors(hostname):
-    a=os.getcwd()
-    os.chdir(a+cors_cmd_str1)
     try:
-        os.remove(cors_filename)
-    except OSError:
-        pass
-    with open(cors_filename, 'w') as outfile:
-        outfile.write(hostname)
-    d=os.popen(cors_cmd_str2).read()
-    try:
-        os.remove(cors_filename)
-    except OSError:
-        pass
-    os.chdir(predir)
+        a=os.getcwd()
+        os.chdir(a+cors_cmd_str1)
+        try:
+            os.remove(cors_filename)
+        except OSError:
+            pass
+        with open(cors_filename, 'w') as outfile:
+            outfile.write(hostname)
+        d=os.popen(cors_cmd_str2).read()
+        try:
+            os.remove(cors_filename)
+        except OSError:
+            pass
+        os.chdir(predir)
+    except Exception as e:
+        print(str(e)+"XContentTypeOptions")
+        d=""
+        try:
+            os.remove(cors_filename)
+        except OSError:
+            pass
     return d
 
 def SQLInjection(hostname):
-    d=os.popen('python2 ./dependency/DTECT1/d-tect.py '+hostname+' |grep "Click Jacking"').read()
-    sqlnjection= (" vulnerable to Click Jacking" in d )
+    try:
+        d=os.popen('python2 ./dependency/DTECT1/d-tect.py '+hostname+' |grep "Click Jacking"').read()
+        sqlnjection= (" vulnerable to Click Jacking" in d )
+    except Exception as e:
+        print(str(e)+"sqlnjection")
+        sqlnjection=False
     return sqlnjection
 
 def borkenACL(hostname):
@@ -125,18 +161,57 @@ def req(d):
     return d
 
 def ArecordRedirection(hostname):
-    txt = dns.resolver.query(hostname, 'A')
-    s=str(txt.response.answer[0].to_text)
-    s=s.replace("<bound method RRset.to_text of <DNS ","").replace(". IN A RRset>>","")
-    ArecordRedirection = (hostname != s)
+    try:
+        txt = dns.resolver.query(hostname, 'A')
+        s=str(txt.response.answer[0].to_text)
+        s=s.replace("<bound method RRset.to_text of <DNS ","").replace(". IN A RRset>>","")
+        ArecordRedirection = (hostname != s)
+    except Exception as e:
+        print(str(e)+"ArecordRedirection")
+        ArecordRedirection=False
     return ArecordRedirection
 
 def blindjacking(hostname):
-    a=os.getcwd()
-    os.chdir(a+"/dependency/findject/")
-    d=os.popen("tshark -i  eth0 -F pcap -w test.pcap").read()
-    d=os.popen("python findject.py test.pcap")
+    try:
+        a=os.getcwd()
+        os.chdir(a+"/dependency/findject/")
+        d=os.popen("tshark -i  eth0 -F pcap -w test.pcap").read()
+        d=os.popen("python findject.py test.pcap")
+    except Exception as e:
+        print(str(e)+"blindjacking")
+        d=""
     return d
-    
+
+@app.route('/main/<hostname>', methods=['GET'])    
+def Main(hostname):
+    try:
+        json_data = {}
+        json_data["domain"]=hostname
+        json_data["xmas"]=xmas(hostname)
+        json_data["XSSProtection"]=XSSProtection(hostname)
+        json_data["XFrameOptions"]=XFrameOptions(hostname)
+        json_data["XContentTypeOptions"]=XContentTypeOptions(hostname)
+        json_data["cors"]=cors(hostname)
+        json_data["blindjacking"]=blindjacking(hostname)
+        json_data["ArecordRedirection"]=ArecordRedirection(hostname)
+        json_data["SQLInjection"]=SQLInjection(hostname)
+        
+    except Exception as e:
+        print(str(e)+"main")
+        json_data = {}
+        json_data["domain"]=hostname
+    return str(json_data).replace("\'", "\"")
+        
+
+
+
+
+
+
+
+
+
      
-    
+if __name__ == "__main__":
+    app.run(host='192.168.35.33',port=7444)
+     
