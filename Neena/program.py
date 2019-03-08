3f1ee80739f9f12501fac62ea69cff60a805d74f
Karan krishna <mail2karankrishnan@gmail.com>
2019-03-08 15:08:18 +0530

    XSS update

--- Neena/program.py
+++ Neena/program.py
@@ -353,7 +353,7 @@ def xsspy(hostname):
         os.chdir(a+"/dependency/XssPy/")
         d=os.popen("python XssPy.py -u "+hostname).read()
         d=d.remove("\n","").split("Started finging XSS")
-        d= (d == "")
+        d= (d == "") or ("No link found, exiting" in d)
         os.chdir(a)
     except:
         d = False
