17dad76c47cc699f8c209c1bb9593e7ed55901a6
Karan krishna <mail2karankrishnan@gmail.com>
2019-04-26 19:04:32 +0530

    updated unitest

--- Neena/test_serverside.py
+++ Neena/test_serverside.py
@@ -85,3 +85,86 @@ def test_XFrameOptions_best():
 def test_XFrameOptions_worst():
     assert serverside.XFrameOptions("www.") == False
     assert serverside.XFrameOptions("") == False
+    
+
+def test_XSSProtection_best():
+    assert serverside.XSSProtection("google.com") == False
+    assert serverside.XSSProtection("ibm.com") == True
+
+def test_XSSProtection_worst():
+    assert serverside.XSSProtection("www.") == False
+    assert serverside.XSSProtection("") == False
+
+
+def test_xmas_best():
+    assert len(serverside.xmas("www.google.com")) == 2
+    assert len(serverside.xmas("google.com")) == 2
+
+def test_xmas_worst():
+    assert serverside.xmas("") == ['']
+
+
+def test_calc_SessionHijack_best():
+    assert serverside.calc_SessionHijack("www.google.com") == '1'
+    assert serverside.calc_SessionHijack("google.com") == '1'
+
+def test_calc_SessionHijack_worst():
+    assert serverside.calc_SessionHijack("") == '1'
+
+
+def test_calc_CORS_best():
+    assert serverside.calc_CORS("www.google.com") == '10'
+    assert serverside.calc_CORS("google.com") == '10'
+
+def test_calc_CORS_worst():
+    assert serverside.calc_CORS("asss") == '10'
+
+
+def test_calc_A_RecordRedirection_best():
+    assert serverside.calc_A_RecordRedirection("www.google.com") == '1'
+    assert serverside.calc_A_RecordRedirection("google.com") == '1'
+
+def test_calc_A_RecordRedirection_worst():
+    assert serverside.calc_A_RecordRedirection("") == '1'
+
+
+def test_calc_calc_SQLInjection_best():
+    assert serverside.calc_SQLInjection("www.google.com") == '1'
+    assert serverside.calc_SQLInjection("google.com") == '1'
+
+def test_calc_SQLInjection_worst():
+    assert serverside.calc_SQLInjection("") == '1'
+
+
+def test_calc_BrokenACL_best():
+    assert serverside.calc_BrokenACL("www.google.com") == '1'
+    assert serverside.calc_BrokenACL("google.com") == '1'
+
+def test_calc_BrokenACL_worst():
+    assert serverside.calc_BrokenACL("") == '1'
+
+
+def test_calc_Authentication_best():
+    assert serverside.calc_Authentication("www.google.com") == '7'
+    assert serverside.calc_Authentication("google.com") == '7'
+
+def test_calc_Authentication_worst():
+    assert serverside.calc_Authentication("www.") == '6'
+    
+
+def test_calc_Xmas_best():
+    assert serverside.calc_Xmas("www.google.com") == '8'
+    assert serverside.calc_Xmas("google.com") == '8'
+
+def test_calc_Xmas_worst():
+    assert serverside.calc_Xmas("www.") == '9'
+
+
+def test_calc_xss_best():
+    assert serverside.calc_xss("www.google.com") == '7'
+    assert serverside.calc_xss("google.com") == 7
+
+def test_calc_xss_worst():
+    assert serverside.calc_xss("www.") == 7
+
+    
